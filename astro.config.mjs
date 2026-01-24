b95a44e4df3f5dea14081ddb366d98065e048ce7
LinCie <aldiofernanda@protonmail.com>
2026-01-24 12:47:07 +0700

    chore: update project configurations and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,10 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: import.meta.env.APP_URL,
   vite: {
     plugins: [tailwindcss()],
   },
-
   integrations: [
     react({
       babel: {
