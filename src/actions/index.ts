4199c36c09147df421a84fc011bfc2d08bc0c1e5
LinCie <aldiofernanda@protonmail.com>
2026-02-04 02:30:36 +0700

    feat(actions): register batchTransactionAction
    - Export batchTransactionAction in server actions
    - Make checkout functionality available to client-side code

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,10 +3,14 @@ import {
   getProductsAction,
   batchReadProductsAction,
 } from "@/modules/products/actions";
+import { batchTransactionAction } from "@/modules/checkout/actions";
 
 export const server = {
   // Products
   getProductsAction: getProductsAction,
   getProductAction: getProductAction,
   batchReadProductsAction: batchReadProductsAction,
+
+  // Checkout
+  batchTransactionAction: batchTransactionAction,
 };
